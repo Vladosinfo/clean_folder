56c5fdf3b9ff932fcf18a6c2fb0e438041e11b60
Vlad <vladyslav.pasko@gmail.com>
2023-11-26 00:18:32 +0200

    Added packages

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-setup(name='cleat-folder',
+setup(name='cleat-folder'
       version='1.0.0',
       description='Structure and reorganize the order in the repository',
       url='https://github.com/Vladosinfo/clean_folder',
       author='Vladyslav',
       author_email='vladosinfo@gmail.com',
       license='MIT',
+      packages=find_namespace_packages(),
       entry_points={'clean-folder': ['helloworld = clean_folder.clean:main']}
 )
